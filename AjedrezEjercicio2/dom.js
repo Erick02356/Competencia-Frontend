e3857c744ad1a4b451eed572b1dab6eeda3605d4
ErickRodriguez <eick02356@gmail.com>
2024-11-11 22:13:08 -0500

    Se avanzo en la logica de movimientos de los peones, con un leve estancamiento

--- AjedrezEjercicio2/dom.js
+++ AjedrezEjercicio2/dom.js
@@ -7,20 +7,19 @@
 
 
 //DESAFIO REALIZAR ESTE ESCALADO EN BURBUJA PERO CON UN WHILE
-
+const piezas = [
+    ['♜','♞','♝','♛','♚','♝','♞','♜'],
+    ['♟','♟','♟','♟','♟','♟','♟','♟'],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['♙','♙','♙','♙','♙','♙','♙','♙'],
+    ['♖','♘','♗','♕','♔','♗','♘','♖'],
+];
 document.addEventListener('DOMContentLoaded',(Event)=>{
 
     const tablero = document.querySelector('#tablero');
-    const piezas = [
-        ['♜','♞','♝','♛','♚','♝','♞','♜'],
-        ['♟','♟','♟','♟','♟','♟','♟','♟'],
-        [],
-        [],
-        [],
-        [],
-        ['♙','♙','♙','♙','♙','♙','♙','♙'],
-        ['♖','♘','♗','♕','♔','♗','♘','♖'],
-    ];
 
     for (let fila = 0; fila < 8; fila++) {        
 
@@ -55,6 +54,7 @@ document.addEventListener('DOMContentLoaded',(Event)=>{
             }
 
             // pieza.addEventListener('click',() => seleccionarFicha(pieza))
+            
             pieza.addEventListener('click',() => cambiarTurno(pieza))
 
         }
@@ -68,58 +68,96 @@ document.addEventListener('DOMContentLoaded',(Event)=>{
 
 const seleccionarFicha = (pieza) =>{
         // cambiarTurno(pieza)
-
+        
         const eliminarSeleccion = document.querySelector('.seleccionado')
         if(eliminarSeleccion){
             eliminarSeleccion.classList.remove('seleccionado')
         }
         pieza.classList.add('seleccionado')
+        pieza.setAttribute("id", "seleccionado");
+        movimientoPeon(pieza);
+
+}
+
+
+const movimientoPeon = (pieza) => {
+ /*
+ 1 .Identificar que la pieza sea un peon
+ 2. Al dar click se deben iluminar las dos siguientes casillas
+ */ 
+    let 
+    elementoBuscado = pieza.textContent;
+    let filaOrigen, indiceElemento;
+    elementoClikeado = pieza.getAttribute('id');
+    if(elementoClikeado=== 'seleccionado'){
+        console.log('Eres un peon')
+
+    //Encontramos la fila en la que se encuentra la pueza que vamos a buscar
+        for (let fila = 0; fila < piezas.length; fila++) { 
+           let indice = piezas[fila].indexOf(elementoBuscado);
+           if (indice != -1){
+            filaOrigen= fila;
+            indiceElemento = indice;
+            console.log(filaOrigen);
+            console.log(indiceElemento);
+            break;
+           }
+        }
+
+
+        console.log(`Aquí se guarda la info del elemento clickeado y es ${elementoClikeado}`)
+
+        
+/* 
+AVANCE:
+
+Esta funcion lo que intenta es encontrar los indices del elemento que fue seleccionado esto con el fin
+de que al dar clic sepamos con exactitud donde estamos situados y en funcion a esto se conozca cuales seran 
+las casillas que deben cambiar de color para habilitar su movimiento
+
+Lo que me tiene frenado es que al haber valores repetidos no puedo conocer el indice exacto del elemento clickeado
+sino que me devuelve el primer elemento siempre [en el caso del peon]
+
+*/
+    }
+
 }
 
 
-const movimientoPeon = () => {
-
-}
 let inicioMovimientos=true;
+
+let parrafo = document.createElement("p");
+parrafo.textContent = "Empiezan las fichas blancas";
+document.body.appendChild(parrafo);
 
 const cambiarTurno = (pieza) =>{
 
     const turno =  pieza.getAttribute('class');
-    console.log(turno)
+    //console.log(turno)
     if(turno === 'pieza fichaBlanca'){
       if(inicioMovimientos){
         //MUEVEN LAS BLANCAS
         inicioMovimientos=!inicioMovimientos    
         seleccionarFicha(pieza)
-        console.log('Mueven las negras')
-        console.log(inicioMovimientos)
+        // console.log('Mueven las negras')
+        // console.log(inicioMovimientos)
+        parrafo.textContent = "Ahora mueven las fichas negras";
+
       }
       return;
     }else{
         if(!inicioMovimientos){
-
             //MUEVEN LAS NEGRAS
             inicioMovimientos=!inicioMovimientos    
+            //console.log('Mueven las blancas')
             seleccionarFicha(pieza)
-            console.log('Mueven las blancas')
-            console.log(inicioMovimientos)
+            parrafo.textContent = "Ahora mueven las fichas blancas";
+
+            //console.log(inicioMovimientos)
         }
       
 
     }
+    document.body.appendChild(parrafo);
+
 }
-
-/*
-PASO A PASO COMO CREO 
-
-    SE ASUME QUE INICIAN LAS BLANCAS
-    BLOQUEAMOS EL CLICK EN LAS NEGRAS
-    AL DAR CLICK EN LAS BLANCAS
-    BLOQUEAMOS LAS BLANCAS
-
-    
-
-*/
-
-
-//SELECCIONAR EN COLOR DISTINTA LAS CASILLAS QUE PUEDE MOVER
